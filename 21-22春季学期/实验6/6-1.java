5c038b953d72c57dad46d8e68835d4d24ca0143c
PennDavid <2996684485@qq.com>
2022-04-28 08:35:38 +0800

    2022-04-28-08-35

--- 21-22春季学期/实验6/6-1.java
+++ 21-22春季学期/实验6/6-1.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
 
-public static void main(String[] args) {
+class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //这边是你的代码
-        sc.close();
+        int n = sc.nextInt();
+        Object []a = new Object[n];
+        for(int i=0; i<n; i++) {
+            String l = sc.next();
+            char k = l.charAt(0);
+            switch (k) {
+                case 'c' :{
+                    a[i] = new Computer();
+                    break;
+                }
+                case 'd' :{
+                    double n1 = sc.nextDouble();
+                    a[i] = n1;
+                    break;
+                }
+                case 'i' :{
+                    int n2 = sc.nextInt();
+                    a[i] = n2;
+                    break;
+                }
+                case 's' :{
+                    String n3 = sc.next();
+                    a[i] = n3;
+                    break;
+                }
+                default:{
+                    a[i] = null;
+                    break;
+                }
+            }
         }
+        for(int i=n-1 ;i>=0; i--) {
+            if(a[i]==null)
+                continue;
+            System.out.println(a[i]);
+        }
+
+        sc.close();
+    }
+
+}
