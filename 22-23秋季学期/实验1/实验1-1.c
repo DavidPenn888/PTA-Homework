e7d8f0cefc29ffcfda3e8df5c438167bc4758213
PennDavid <2996684485@qq.com>
2022-10-27 16:06:07 +0800

    更新本学期实验

--- 22-23秋季学期/实验1/实验1-1.c
+++ 22-23秋季学期/实验1/实验1-1.c
@@ -1,1 +1,52 @@
-//暂无
+List MakeEmpty()
+{
+    List L=(List)malloc(MAXSIZE * sizeof(struct LNode));
+    L->Last=-1;
+    return L;
+}
+ 
+Position Find( List L, ElementType X )
+{
+    Position p = -1;
+    for(int i = 0;i <= L -> Last; i++)
+    {
+        if(X == L -> Data[i]) p = i;
+    }
+    if(p == -1)return ERROR;
+    else return p;
+}
+ 
+bool Insert( List L, ElementType X, Position P )
+{
+    if(L->Last>=MAXSIZE-1)
+    {
+        printf("FULL");
+        return false;
+    }
+    if(P<0 || P>L -> Last+1)
+    {
+        printf("ILLEGAL POSITION");
+        return false;
+    }
+    
+    for(int i = L -> Last + 1;i > P;i--) L->Data[i]=L->Data[i-1];
+    L->Data[P]=X;
+    L->Last++;
+    return true;
+}
+ 
+bool Delete( List L, Position P )
+{
+    if(P<0||P>L->Last)
+    {
+        printf("POSITION %d EMPTY",P);
+        return false;
+    }
+    
+    for(int i=P;i<L->Last;i++)
+    {
+        L->Data[i]=L->Data[i+1];
+    }
+    L->Last--;
+    return true;
+}
