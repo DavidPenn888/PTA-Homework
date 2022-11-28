115610f18f65bd938b5ead29108f2e7d9e596b7c
PennDavid <2996684485@qq.com>
2022-11-28 10:16:24 +0000

    更新本学期实验
    Signed-off-by: PennDavid <2996684485@qq.com>

---
+++ 22-23秋季学期/实验8/8-2.c
@@ -0,0 +1,16 @@
+void selectionSort(int arr[], int n)
+{
+    int i,j,k,t;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                t=arr[i];
+                arr[i]=arr[j];
+                arr[j]=t;
+            }
+        }
+    }
+}
